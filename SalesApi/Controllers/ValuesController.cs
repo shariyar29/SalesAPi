97659b9b369aa496cbab37d6f10461c528d56002
shariyar shaikh <shariyar_29@hotmail.com>
2020-09-05 11:21:44 +0530

    Add project files.

---
+++ SalesApi/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using SalesApi.Models;
+
+namespace SalesApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+       
+        DBSalesChartEntities db = new DBSalesChartEntities();   
+        // GET api/values
+        [Route("api/sales/")]
+        public IHttpActionResult Get()
+        {
+            var result = db.sp_sales().ToList();
+            return Ok(result);
+        }
+        [Route("api/allsales/")]
+        [HttpGet]
+        public IHttpActionResult allsales()
+        {
+            var result = db.sp_allsales().ToList();
+            return Ok(result);
+        }
+
+        [Route("api/manager/")]
+        [HttpGet]
+        public IHttpActionResult Manager()
+        {
+            var result = db.sp_manager().ToList();
+            return Ok(result);
+        }
+
+        [Route("api/newemp")]
+        [HttpPost]
+        public void post([FromBody]tbl_employee employee)
+        {
+            using (DBSalesChartEntities db = new DBSalesChartEntities())
+            {
+                db.tbl_employee.Add(employee);
+                db.SaveChanges();
+            }
+
+
+        }
+
+    }
+}
---
+++ SalesApi/Models/Employeeclass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalesApi.Models
+{
+    public class Employessclass
+    {
+        public int EmpID { get; set; }
+        public string FirstName { get; set; }
+        public string lastName { get; set; }
+        public string TotalAmount { get; set; }
+        public string ManagerName { get; set; }
+    }
+}
